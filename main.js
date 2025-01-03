a2f4d90b7c03be7c19df9aabc32b46107ba16904
Bilalshabbir6 <bilalalig637@gmail.com>
2025-01-03 18:46:40 +0500

    uploaded code

---
+++ main.js
@@ -0,0 +1,27 @@
+document.write("hello world my first javascript file <br>");
+document.write(12345, "<br>");
+
+console.log("hello world my first javascript file");
+console.log(12345);
+
+// syntax error
+// runtime error
+// logical error
+
+document.write(typeof "hello", "<br>");
+document.write(typeof 123, "<br>");
+
+document.write("hello world".length, "<br>");
+document.write("hello world".toUpperCase(), "<br>");
+document.write("hello world".toLowerCase(), "<br>");
+
+// document.write("hello" + "world");
+document.write("add = ", 15 + 5, "<br>")
+document.write("sub = ", 15 - 5, "<br>")
+document.write("multiply = ", 15 * 5, "<br>")
+document.write("divide = ", 15 / 5, "<br>")
+document.write("divide = ", 17 % 5, "<br>")
+
+document.write(toString(10.10))
+document.write(parseFloat(10.10))
+document.write(parseInt(10.10))
